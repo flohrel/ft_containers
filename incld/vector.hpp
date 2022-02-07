0b8fb90eb6f5c71b5908c9de015ad0deed7c876b
Florian OHREL <flohrel@student.42.fr>
2022-02-07 19:21:03 +0100

    .

--- incld/vector.hpp
+++ incld/vector.hpp
@@ -1,8 +1,11 @@
 #ifndef __VECTOR__H__
-#define __VECTOR__H__
+# define __VECTOR__H__
+
+# include <memory>
 
 namespace ft
 {
+	template < typename T, typename Alloc = std::allocator< T > >
 	class vector
 	{
 		public:
@@ -11,32 +14,32 @@ namespace ft
 			~vector( void );
 	
 			vector &	operator=( vector const & rhs );
-	
+		
+		
+		vector::vector( void )
+		{
+			/* Content */
+			return;
+		}
+		
+		vector::vector( vector const & src )
+		{
+			*this = src;
+			return;
+		}
+		
+		vector::~vector( void )
+		{
+			/* Content */
+			return;
+		}
+		
+		vector &	vector::operator=( vector const & rhs )
+		{
+			/* Content */
+			return (*this);
+		}
 	};
-	
-	vector::vector( void )
-	{
-		/* Content */
-		return;
-	}
-	
-	vector::vector( vector const & src )
-	{
-		*this = src;
-		return;
-	}
-	
-	vector::~vector( void )
-	{
-		/* Content */
-		return;
-	}
-	
-	vector &	vector::operator=( vector const & rhs )
-	{
-		/* Content */
-		return (*this);
-	}
 }
 
 #endif
