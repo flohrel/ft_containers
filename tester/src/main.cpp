f5c6ac58a906dab5b36e7d7a89ab818a2ec90da2
flohrel <flohrel@student.42.fr>
2022-04-25 04:00:40 +0200

    ADD map suite

--- tester/src/main.cpp
+++ tester/src/main.cpp
@@ -5,7 +5,7 @@ int	main( void )
 	unit_test::TestRunner	runner;
 
 	unit_test::vector_suite::suite_registrar();
-	// unit_test::stack_suite();
+	unit_test::map_suite::suite_registrar();
 	// map_suite();
 	// set_suite();
 
