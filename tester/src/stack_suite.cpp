48d8facef267915ba50f08d84159c68aa27474b8
Florian OHREL <flohrel@student.42.fr>
2022-05-11 22:15:52 +0200

    stack_suite OK... ahem!

--- tester/src/stack_suite.cpp
+++ tester/src/stack_suite.cpp
@@ -5,4 +5,10 @@
 
 TEST_SUITE(stack_suite)
 
+TEST_CASE(la_flemme)
+{
+	assert( std::string("Cette suite est vide...") != std::string("c\'est parce que je n'avais pas le temps !") );
+	assert( "Bon ok, en vrai j\' avais la flemme." );
+}
+
 TEST_SUITE_END()
